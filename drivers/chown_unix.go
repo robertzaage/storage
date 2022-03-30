cf49936531d7c56eb74ee5934e28d610293f1c12
robertzaage <38880514+robertzaage@users.noreply.github.com>
2022-03-30 08:30:26 +0200

    handle EOVERFLOW and don't return error

--- drivers/chown_unix.go
+++ drivers/chown_unix.go
@@ -86,6 +86,9 @@ func (c *platformChowner) LChown(path string, info os.FileInfo, toHost, toContai
 		cap, err := system.Lgetxattr(path, "security.capability")
 		if err != nil && !errors.Is(err, system.EOPNOTSUPP) && err != system.ErrNotSupportedPlatform {
 			return fmt.Errorf("%s: %v", os.Args[0], err)
+		} else if errors.Is(err, system.EOVERFLOW) {
+			fmt.Printf("%s: %v\n", os.Args[0], err)
+			return nil
 		}
 
 		// Make the change.
